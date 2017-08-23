c1156ab6a7b06e80cb1611eeae4b4a4090d05e8c
Rei Araki <araki@welyco.com>
2017-08-23 10:17:32 +0900

    fix

--- blescan.py
+++ blescan.py
@@ -173,8 +173,7 @@ def parse_events(sock, loop_count=100):
     		    Adstring += "%i" % struct.unpack("b", pkt[report_pkt_offset -2])
     		    Adstring += ","
     		    Adstring += "%i" % struct.unpack("b", pkt[report_pkt_offset -1])
-		    #print "\tAdstring=", Adstring
- 		    myFullList.append(Adstring)
-                done = True
+            myFullList.append(Adstring)
+            done = True
     sock.setsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, old_filter )
     return myFullList
