0d51f9db16e065f58a11a2432cee5171ebe2ec8e
Rei Araki <araki@welyco.com>
2017-08-23 10:35:50 +0900

    fix

--- blescan.py
+++ blescan.py
@@ -162,7 +162,7 @@ def parse_events(sock, loop_count=100):
 		    # build the return string
                     Adstring = packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
 		    Adstring += ","
-		    # Adstring += returnstringpacket(pkt[report_pkt_offset -22: report_pkt_offset - 6])
+		    Adstring += struct.unpack('b', pkt[report_pkt_offset - 16: report_pkt_offset - 15])[0]
 		    # Adstring += ","
 		    # Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4])
 		    # Adstring += ","
